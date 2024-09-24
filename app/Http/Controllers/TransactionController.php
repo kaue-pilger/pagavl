94d486a975cc72144f5245c6b9be66b5cd0d4302
Kaue Pilger <kauesouzapilger@gmail.com>
2024-09-24 17:58:47 +0200

    :rocket: feat: APIs, Model and Controller created

---
+++ app/Http/Controllers/TransactionController.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Transaction;
+use Illuminate\Http\Request;
+
+class TransactionController extends Controller
+{
+    public function index()
+    {
+        return Transaction::all();
+    }
+
+    public function store(Request $request)
+    {
+        $transaction = Transaction::create($request->all());
+        return response()->json($transaction, 201);
+    }
+
+    public function show(Transaction $transaction)
+    {
+        return $transaction;
+    }
+
+    public function update(Request $request, Transaction $transaction)
+    {
+        $transaction->update($request->all());
+        return response()->json($transaction, 200);
+    }
+
+    public function destroy(Transaction $transaction)
+    {
+        $transaction->delete();
+        return response()->json(null, 204);
+    }
+}
---
+++ app/Models/Transaction.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Transaction extends Model
+{
+  use HasFactory;
+
+  protected $fillable = [
+    'amount',
+    'currency',
+    'payment_method',
+    'merchant_id',
+    'order_id',
+    'date'
+  ];
+
+}
