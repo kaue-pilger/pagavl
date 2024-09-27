04a6d809aa40f4a8eac4946135ab75e69952eb53
Kaue Pilger <kauesouzapilger@gmail.com>
2024-09-27 22:29:54 +0200

    :boom: fix: Move transaction logic to Model

--- app/Http/Controllers/TransactionController.php
+++ app/Http/Controllers/TransactionController.php
@@ -4,64 +4,86 @@ namespace App\Http\Controllers;
 
 use App\Models\Transaction;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Log;
 use Inertia\Inertia;
 
 class TransactionController extends Controller
 {
-    public function index()
-    {
-        return Transaction::all();
-    }
+  public function index()
+  {
+    $transactions = Transaction::all();
 
-    public function create() 
-    {
-        $transactions = Transaction::all();
-        return Inertia::render('Transactions/Index', [
-            'transactions' => $transactions,
-        ]);
-    }
+    return response()->json($transactions);
+  }
 
-    public function store(Request $request)
-    {
-        $transaction = Transaction::create($request->all());
-        return response()->json($transaction, 201);
-    }
-
-    public function show(Transaction $transaction)
-    {
-        return $transaction;
-    }
-
-    public function getTotalsByCurrency()
-    {
-        $totals = Transaction::select('currency', DB::raw('SUM(amount) as total'))
-            ->groupBy('currency')
-            ->get();
+  public function create() 
+  {
+    $transactions = Transaction::all();
     
-        return response()->json($totals);
-    }
+    return Inertia::render('Transactions/Index', [
+        'transactions' => $transactions,
+    ]);
+  }
 
-    public function getTotalsByPaymentMethods()
-    {
-        $payment_methods = Transaction::select('payment_method', DB::raw('COUNT(*) as total'))
-            ->groupBy('payment_method')
-            ->get()
-            ->pluck('total', 'payment_method'); // Pluck to create the key-value format
-        
-        return response()->json($payment_methods);
-    }
-    
+  public function store(Request $request)
+  {
+    $validatedRequest = $request->validate([
+      'amount' => 'required|numeric',
+      'currency' => 'required|string',
+      'payment_method' => 'required|numeric',
+      'merchant_id' => 'required|numeric',
+      'order_id' => 'required|string'
+    ]);
 
-    public function update(Request $request, Transaction $transaction)
-    {
-        $transaction->update($request->all());
-        return response()->json($transaction, 200);
-    }
+    $transaction = Transaction::create($validatedRequest);
+    return response()->json($transaction, 201);
+  }
 
-    public function destroy(Transaction $transaction)
+  public function show(int $id)
+  {
+    $transactionById = Transaction::find($id);
+
+    if (!$transactionById) 
+      return response()->json(['message' => 'Transaction not found'], 404);
+
+    return response()->json($transactionById);
+  }  
+
+  public function update(Request $request, Transaction $transaction)
+  {
+    $transaction->update($request->all());
+
+    return response()->json($transaction);
+  }
+
+  public function destroy(Transaction $transaction)
+  {
+    $transaction->delete();
+
+    return response()->json(null, status: 204);
+  }
+
+  public function getTotalsByCurrency()
+  {
+    try {
+      return response()->json(Transaction::getTotalsByCurrency());
+    } 
+    catch (\Exception $e) 
     {
-        $transaction->delete();
-        return response()->json(null, 204);
+      Log::error('Failed to fetch, error message: ' . $e->getMessage());
+      return response()->json(['error' => 'Failed to fetch totals by currency'], 500);
     }
+  }   
+
+  public function getTotalsByPaymentMethods()
+  {
+    try {
+      return response()->json(Transaction::getTotalsByPaymentMethods());
+    } 
+    catch (\Exception $e) 
+    {
+      Log::error('Failed to fetch, error message: ' . $e->getMessage());
+      return response()->json(['error' => 'Failed to fetch totals by payment method'], 500);
+    }
+  }
 }
--- app/Models/Transaction.php
+++ app/Models/Transaction.php
@@ -4,18 +4,31 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\DB;
 
 class Transaction extends Model
 {
   use HasFactory;
 
-  protected $fillable = [
-    'amount',
-    'currency',
-    'payment_method',
-    'merchant_id',
-    'order_id',
-    'date'
-  ];
+  protected $fillable = ['amount', 'currency', 'payment_method', 'merchant_id', 'order_id', 'date'];
+
+  public static function getTotalsByCurrency()
+  {
+    $totalsByCurrency = Transaction::select('currency', DB::raw('SUM(amount) as total'))
+      ->groupBy('currency')
+      ->get();
+
+    return $totalsByCurrency;
+  }
+  
+  public static function getTotalsByPaymentMethods()
+  {
+      $totalsByPaymentMethods = Transaction::select('payment_method', DB::raw('COUNT(*) as total'))
+        ->groupBy('payment_method')
+        ->get()
+        ->pluck('total', 'payment_method'); // Pluck to create the key-value format
+      
+      return $totalsByPaymentMethods;
+  }
 
 }
